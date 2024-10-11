4205a4cf0caa6d9731b26c0ae0ea23bb9ea3ca16
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-11 12:42:59 -0400

    commit

---
+++ JS/to-do/js-todo.js
@@ -0,0 +1,44 @@
+const todoList = [{
+    name: '',
+    dueDate: ''
+}];
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
+  }
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+    const inputElem = document.querySelector(".js-name-input");
+    const name = inputElem.value;
+
+    const dateElem = document.querySelector(".js-due-date-input");
+    const dueDate = dateElem.value;
+
+    todoList.push({
+        name, //  name: name,
+        dueDate // dueDate: dueDate 
+    });
+    inputElem.value = '';
+
+    renderToDoList();
+}
