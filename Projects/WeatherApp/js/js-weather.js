a530c6d6a99e069a43b78a84c5b9327ad589bfa1
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-12 23:20:25 -0400

    date-library

--- Projects/WeatherApp/js/js-weather.js
+++ Projects/WeatherApp/js/js-weather.js
@@ -3,3 +3,5 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 
 const searchBox = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-button");
+
+const today = dayjs().format('dddd H');
