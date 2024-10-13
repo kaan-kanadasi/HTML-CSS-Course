ff390e61a21a271f2f2b69b1a12e3c6d6202cd7f
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-12 23:29:58 -0400

    corrected

--- Projects/WeatherApp/js/js-weather.js
+++ Projects/WeatherApp/js/js-weather.js
@@ -1,3 +1,5 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
 const apiKey = "27742cdbc5018245c73fc948b357e609";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
 
