ea8238051879ed01d1571e6a740912b06a1a1e57
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-17 11:54:21 -0400

    corrected

--- Cammelie-Mormal/js/script.js
+++ Cammelie-Mormal/js/script.js
@@ -15,13 +15,22 @@ window.onmousemove = e => {
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
     const maxDelta = window.innerWidth / 2;
     const percentage = (mouseDelta / maxDelta) * -100;
-    const nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+    let nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
+    nextPercentage = Math.min(nextPercentage, 0);
+    nextPercentage = Math.max(nextPercentage, -100);
+    
     track.dataset.percentage = nextPercentage;
 
-    track.style.transform = `translate(${percentage}%, -50%)`;
-
+    // track.style.transform = `translate(${nextPercentage}%, -50%)`;
+    track.animate({
+        transform:`translate(${nextPercentage}%, -50%)`
+    }, {duration: 1200, fill: "forwards"});
+    
     for(const image of track.getElementsByClassName('img')) {
-        image.style.objectPosition = `${nextPercentage + 100} 50%`;
+        // image.style.objectPosition = `${nextPercentage + 100}% 50%`;
+        image.animate({
+            objectPosition: `${nextPercentage + 100}% center`
+        }, {duration: 1200, fill: "forwards"});
     }
 }
